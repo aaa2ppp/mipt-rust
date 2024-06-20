46f5c91459b1962745fcd2b3aaea017cfb733801
Aleksandr Panfilov <aaa2ppp@gmail.com>
2024-06-20 22:22:12 +0300

    problems/intro/combinations done

--- problems/intro/combinations/src/lib.rs
+++ problems/intro/combinations/src/lib.rs
@@ -1,6 +1,35 @@
 #![forbid(unsafe_code)]
 
 pub fn combinations(arr: &[i32], k: usize) -> Vec<Vec<i32>> {
-    // TODO: your code goes here.
-    unimplemented!()
+    let mut res: Vec<Vec<i32>> = Vec::new();
+
+    if k <= arr.len() {
+        let mut lug = Lug {
+            arr,
+            buf: &mut vec![0; k][..],
+            res: &mut res,
+        };
+
+        dfs(&mut lug, 0, 0, arr.len() - k);
+    }
+
+    return res;
+}
+
+struct Lug<'a> {
+    arr: &'a [i32],
+    buf: &'a mut [usize],
+    res: &'a mut Vec<Vec<i32>>,
+}
+
+fn dfs(lug: &mut Lug, i: usize, start: usize, end: usize) {
+    if i == lug.buf.len() {
+        lug.res.push(lug.buf.iter().map(|j| lug.arr[*j]).collect());
+        return;
+    }
+
+    for j in start..=end {
+        lug.buf[i] = j;
+        dfs(lug, i + 1, j + 1, end + 1)
+    }
 }
---
+++ problems/intro/combinations_v2/src/lib.rs
@@ -0,0 +1,30 @@
+#![forbid(unsafe_code)]
+
+pub fn combinations(arr: &[i32], k: usize) -> Vec<Vec<i32>> {
+    let mut res: Vec<Vec<i32>> = Vec::new();
+
+    if k <= arr.len() {
+        let mut buf: Vec<usize> = vec![0; k];
+        let mut i = 0;
+        let mut j = 0;
+        let  n = arr.len() - k;
+
+        loop {
+            while i < buf.len() && j <= n + i {
+                buf[i] = j;
+                j += 1;
+                i += 1;
+            }
+            if i == buf.len() {
+                res.push(buf.iter().map(|j| arr[*j]).collect());
+            } 
+            if i == 0 {
+                break;
+            }
+            i -= 1;
+            j = buf[i] + 1;
+        }
+    }
+
+    return res;
+}
