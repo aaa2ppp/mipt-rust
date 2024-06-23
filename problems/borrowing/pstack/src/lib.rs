be4f61861621c02164a5b3fbc9a9e189af4fdec8
Aleksandr Panfilov <aaa2ppp@gmail.com>
2024-06-23 20:53:21 +0300

    problems/borrowing/pstack done

--- problems/borrowing/pstack/src/lib.rs
+++ problems/borrowing/pstack/src/lib.rs
@@ -1,68 +1,97 @@
 #![forbid(unsafe_code)]
 use std::rc::Rc;
 
+struct Node<T> {
+    value: T,
+    prev: Option<Rc<Node<T>>>,
+}
+
 pub struct PRef<T> {
-    // TODO: your code goes here.
+    node: Rc<Node<T>>,
 }
 
 impl<T> std::ops::Deref for PRef<T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        // TODO: your code goes here.
-        unimplemented!()
+        &self.node.value
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-
 pub struct PStack<T> {
-    // TODO: your code goes here.
+    top: Option<Rc<Node<T>>>,
+    len: usize,
 }
 
 impl<T> Default for PStack<T> {
     fn default() -> Self {
-        // TODO: your code goes here.
-        unimplemented!()
+        Self { top: None, len: 0 }
     }
 }
 
 impl<T> Clone for PStack<T> {
     fn clone(&self) -> Self {
-        // TODO: your code goes here.
-        unimplemented!()
+        Self {
+            top: self.top.clone(),
+            len: self.len,
+        }
     }
 }
 
 impl<T> PStack<T> {
     pub fn new() -> Self {
-        // TODO: your code goes here.
-        unimplemented!()
+        Self::default()
     }
 
     pub fn push(&self, value: T) -> Self {
-        // TODO: your code goes here.
-        unimplemented!()
+        let node = Rc::new(Node {
+            prev: self.top.clone(),
+            value,
+        });
+        Self {
+            top: Some(node),
+            len: self.len + 1,
+        }
     }
 
     pub fn pop(&self) -> Option<(PRef<T>, Self)> {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.top.clone().map(|node| {
+            let tail = Self {
+                top: node.prev.clone(),
+                len: self.len - 1,
+            };
+            (PRef { node }, tail)
+        })
     }
 
     pub fn len(&self) -> usize {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.len
     }
 
     pub fn is_empty(&self) -> bool {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.len == 0
     }
 
-    pub fn iter(&self) -> impl Iterator<Item = PRef<T>> {
-        // TODO: your code goes here.
-        unimplemented!()
+    pub fn iter(&self) -> PStackIterator<T> {
+        PStackIterator {
+            stack: self.clone(),
+        }
     }
 }
 
+pub struct PStackIterator<T> {
+    stack: PStack<T>,
+}
+
+impl<T> Iterator for PStackIterator<T> {
+    type Item = PRef<T>;
+
+    fn next(&mut self) -> Option<<Self as Iterator>::Item> {
+        if let Some((top, tail)) = self.stack.pop() {
+            self.stack = tail;
+            return Some(top);
+        }
+        None
+    }
+}
