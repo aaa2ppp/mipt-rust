4366137c2fd3d3eb65ec0f622dc6287acf2707df
Aleksandr Panfilov <aaa2ppp@gmail.com>
2024-06-24 15:49:16 +0300

    problems/borrowing/context done

--- problems/borrowing/context/src/lib.rs
+++ problems/borrowing/context/src/lib.rs
@@ -1,17 +1,38 @@
 #![forbid(unsafe_code)]
 
+use std::any::{Any, TypeId};
+use std::collections::HashMap;
+
 pub struct Context {
-    // TODO: your code goes here.
+    objs: HashMap<String, Box<dyn Any>>,
+    singles: HashMap<TypeId, Box<dyn Any>>,
 }
 
 impl Context {
     pub fn new() -> Self {
-        // TODO: your code goes here.
-        unimplemented!()
+        Context {
+            objs: HashMap::new(),
+            singles: HashMap::new(),
+        }
     }
 
-    // fn insert(&mut self, key: ..., obj: ...);
-    // fn get(&self, key: ...);
-    // fn insert_singletone(&mut self, obj: ...);
-    // fn get_singletone(&self);
+    pub fn insert<K: ToString, V: Any>(&mut self, key: K, obj: V) {
+        self.objs.insert(key.to_string(), Box::new(obj));
+    }
+
+    pub fn get<T: Any>(&self, key: &str) -> &T {
+        self.objs.get(key).unwrap().downcast_ref::<T>().unwrap()
+    }
+
+    pub fn insert_singletone<T: Any>(&mut self, obj: T) {
+        self.singles.insert(obj.type_id(), Box::new(obj));
+    }
+
+    pub fn get_singletone<T: Any>(&self) -> &T {
+        self.singles
+            .get(&TypeId::of::<T>())
+            .unwrap()
+            .downcast_ref::<T>()
+            .unwrap()
+    }
 }
